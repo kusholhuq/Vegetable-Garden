eacfe132c2216ee7836cec19d9aa7de375cf3f34
Mohsinul Huq <kusholhuq@gmail.com>
2020-07-06 13:54:32 -0700

    code signal

---
+++ Code Signal/arcade/arrayChange.js
@@ -0,0 +1,24 @@
+function arrayChange(inputArray) {
+  //questions i need to ask
+  //is it increasing? is a less than b, is b less than c, is c less than d
+  //when this is not the case, add to the next one
+  //ie: is a less than b? no? add one to b, and add one to moves
+  //you will have to keep running this loop until its done.
+  //will need to global variables, one is moves, one is done
+  //whenever a move is made, done is turned to false
+  // at the beginning of a loop done is turned to true
+  let done = false;
+  let moves = 0;
+  while (done === false) {
+    done = true;
+    for (let k = 0; k < inputArray.length; k++) {
+      if (inputArray[k] >= inputArray[k + 1]) {
+        //might need ot add temp
+        inputArray.splice(k + 1, 1, inputArray[k + 1] + 1);
+        moves++;
+        done = false;
+      }
+    }
+  }
+  return moves;
+}
