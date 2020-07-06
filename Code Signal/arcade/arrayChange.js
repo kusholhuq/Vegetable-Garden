b06ee7a385316b60f3e7ce35918750dbdff876a2
Mohsinul Huq <kusholhuq@gmail.com>
2020-07-06 14:11:49 -0700

    faster solution

--- Code Signal/arcade/arrayChange.js
+++ Code Signal/arcade/arrayChange.js
@@ -7,18 +7,30 @@ function arrayChange(inputArray) {
   //will need to global variables, one is moves, one is done
   //whenever a move is made, done is turned to false
   // at the beginning of a loop done is turned to true
-  let done = false;
-  let moves = 0;
-  while (done === false) {
-    done = true;
-    for (let k = 0; k < inputArray.length; k++) {
-      if (inputArray[k] >= inputArray[k + 1]) {
-        //might need ot add temp
-        inputArray.splice(k + 1, 1, inputArray[k + 1] + 1);
-        moves++;
-        done = false;
-      }
+  // let done=false;
+  // let moves=0;
+  // while(done===false){
+  //     done=true;
+  //     for (let k=0;k<inputArray.length;k++){
+  //         if(inputArray[k]>=inputArray[k+1]){
+  //             //might need ot add temp
+  //             let temp=inputArray[k+1]+1;
+  //             inputArray.splice(k+1,1,temp);
+  //             moves++;
+  //             done=false;
+  //         }
+  //     }
+  // }
+  // return moves;
+  //too slow
+  let count = 0;
+  for (let k = 0; k < inputArray.length; k++) {
+    if (inputArray[k] >= inputArray[k + 1]) {
+      let difference = inputArray[k] + 1 - inputArray[k + 1];
+      let newNumber = inputArray[k + 1] + difference;
+      inputArray.splice(k + 1, 1, newNumber);
+      count += difference;
     }
   }
-  return moves;
+  return count;
 }
