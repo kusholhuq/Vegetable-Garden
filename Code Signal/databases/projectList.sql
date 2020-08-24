40c3132cd9b99cdab472ef1cc722839007534e29
Mohsinul Huq <kusholhuq@gmail.com>
2020-08-24 10:08:46 -0700

    return 3 columns from a table and order by id

---
+++ Code Signal/databases/projectList.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE projectList()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+
+    SELECT project_name,team_lead,income
+    FROM Projects
+    ORDER BY internal_id;
+
+END
