e07f13eb2d6d3f4cedb49809b205f813ef235e08
Mohsinul Huq <kusholhuq@gmail.com>
2020-10-25 00:41:41 -0700

    smallest and biggest numbers in an array

---
+++ Edabit/Beginner/smallestBiggest.js
@@ -0,0 +1,13 @@
+function minMax(arr) {
+  let min = 10000;
+  let max = -10000;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return [min, max];
+}
