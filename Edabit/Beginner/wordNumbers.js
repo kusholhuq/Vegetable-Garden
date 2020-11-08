2a779d9754ff56f1ce24b61fabbd5d241edf3270
Mohsinul Huq <kusholhuq@gmail.com>
2020-11-07 21:39:17 -0800

    a switch statement

---
+++ Edabit/Beginner/wordNumbers.js
@@ -0,0 +1,24 @@
+function word(s) {
+	switch(s){
+		case "one":
+			return 1;
+		case "two":
+			return 2;
+		case "three":
+			return 3;
+		case "four":
+			return 4;
+		case "five":
+			return 5;
+		case "six":
+			return 6;
+		case "seven":
+			return 7;
+		case "eight":
+			return 8;
+		case "nine":
+			return 9;
+		case "zero":
+			return 0;
+	}
+}
