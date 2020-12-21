a9ac8d3d84d748057ae1387c3e6a0befc883fb04
Mohsinul Huq <kusholhuq@gmail.com>
2020-12-20 22:24:38 -0800

    fixed

--- HackerRank/any-size-matrix.js
+++ HackerRank/any-size-matrix.js
@@ -1,15 +1,15 @@
 function diagonalDifference(arr) {
   // Write your code here
-  let left = arr[0][0] + arr[1][1] + arr[2][2]
+  let left = 0;
   for (let i = 0; i < arr.length; i++) {
     left = left + arr[i][i];
-    console.log(arr[i][i])
+    console.log("left Diagonal", arr[i][i])
   }
   console.log(left);
   let right = 0;
   for (let b = 0; b < arr.length; b++) {
     right = right + arr[b][arr.length - 1 - b];
-    console.log(arr[b][arr.length - 1 - b])
+    console.log("right Diagonal", arr[b][arr.length - 1 - b])
   }
   console.log(right);
   const result = left - right;
